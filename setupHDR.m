4c9f688a02a4ec1bb64bd9fb866ae5d0f8364890
Keith Maki <maki@mumble-19.cs.wisc.edu>
2014-02-13 15:43:23 -0600

    Fleshed out T, w, L portions of setupHDR.m

--- setupHDR.m
+++ setupHDR.m
@@ -3,15 +3,18 @@
 % taken from an nxn grid centered on each of the images
 % And an array of j exposure lengths
 % Also maybe some other things
-function [ imgs, Z, T ] = setupHDR(fnames, n)
-    
-    i = 1;
+function [ imgs, Z, T, L, W ] = setupHDR(fnames, n)
+    assert(iscellstr(fnames), 'Error: fnames must be a cell array of filename strings');
+        
+    Z = zeros(n*n,numel(fnames));
+    W = zeros(n*n,numel(fnames));
     imgs = zeros(size(fnames,1));
+    i = 1;
     for name = fnames
         img = imread(name);
         imgs(i)=img;
         
-        % set up Z(:,j) by extracting nxn grid of pixels in hsl format
+        % set up Z(:,i) by extracting nxn grid of pixels in hsl format
         pxd = size(img)/6;
         crds(1) = pxd(1)*(1:n);
         crds(2) = pxd(2)*(1:n);
@@ -21,10 +24,26 @@ function [ imgs, Z, T ] = setupHDR(fnames, n)
                 Z(n*x+y,i) = tmp(3);
             end
         end
-        i = i+1;
+                
+        % set up T(i) based on image metadata (exif tag)
+        info = imfinfo(name);
+        T(i) = info.DigitalCamera.ExposureTime;
         
-        % set up T(i) based on image metadata? (exif tag)
-        %    code here
+        % choose w(j,i) according to distance from max,min
+        % w(j,i) = z-Zmin for z close to Zmin
+        % w(j,i) = Zmax-z for z close to Zmax
+        j=1;
+        for px = Z(:,i)
+            if px > 128 
+                W(j,i) = 255-px;
+            else
+                W(j,i) = px;
+            end
+        end
+        %Next image
+        i = i+1;
     end
+    
+    L = 1;
     
 end
