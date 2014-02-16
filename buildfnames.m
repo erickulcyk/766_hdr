cf1cfcc1f50762f15a76d6dc6d9425fc6383874c
Eric Kulcyk <erickulcyk@gmail.com>
2014-02-16 17:06:31 -0600

    .hdr files to compare

--- buildfnames.m
+++ buildfnames.m
@@ -5,4 +5,6 @@
 %outputs a cell array of form '{basedir}{shotnum}-{1:n}.jpg'
 function fnames = buildfnames(basedir,shotnum,n)
     for i = 1:n
-        fnames{i} = {[base, num2str(shotnum), '-', num2str(i), '.jpg'};
+        fnames{i} = [basedir, num2str(shotnum), '-', num2str(i), '.jpg'];
+    end
+end
--- phists.m
+++ phists.m
@@ -1,8 +1,8 @@
 function phists(img)
     figure('Name','Red histogram');
-    imhist(img(:,:,1));
+    hist(img(:,:,1));
     figure('Name','Blue histogram');
-    imhist(img(:,:,3));
+    hist(img(:,:,3));
     figure('Name','Green histogram');
-    imhist(img(:,:,2));
+    hist(img(:,:,2));
     
