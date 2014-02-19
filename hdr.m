16f187c9ca85f40f1ea9a2a944689995a95e1b82
Keith Maki <maki@mumble-18.cs.wisc.edu>
2014-02-19 00:31:13 -0600

    Changed some structure things to account for other changes

--- hdr.m
+++ hdr.m
@@ -4,57 +4,59 @@
 % weight = weight of each brightness (0..255)
 % channel = channel to apply hdr to.
 % returns a single hdr image.
-function [ hdr_img ] = hdr( hdr_img, imgs, Time, g, weight, channel)
-
-    %o = squeeze(imgs(1,:,:,:));
-    %disp(size(o));
-    %imshow(o);
+function [ hdr_img ] = hdr( hdr_img, imgs, refind, Time, g, weightfunc, channel, useRef, ghostthresh)
+    
     img_size = size(imgs,1);
     img_w = size(imgs,2);
     img_h = size(imgs,3);
-    %channels = size(imgs,4);
+    Irr = zeros(size(imgs,2),size(imgs,3),size(imgs,1));
     
-    %hdr_img = zeros(img_w,img_h, channels);
+    %Find the log irradiance values for each image
+    IrrRef(:,:) = FindLogIrr(squeeze(imgs(refind,:,:,channel)),Time(refind),g);
+    for k=1:img_size
+        img(:,:) = squeeze(imgs(k,:,:,channel));
+        Irr(:,:,k) = FindLogIrr(img(:,:),Time(k),g);
+        disp(['Image ',num2str(k) ,' max difference ', num2str(max(max(squeeze(Irr(:,:,k))-IrrRef(:,:))))]);
+    end
+    disp('Found log irradiances.  Weighting images...');
+    
+    % get ready to construct hdr values for this channel
+    hdr_img(:,:,channel) = zeros(size(hdr_img(:,:,channel)));
+    weight = zeros(size(imgs,2),size(imgs,3));
     weight_total = zeros(img_w,img_h);
-    for i=1:img_w
-        %disp(i);
-        for j=1:img_h
-            hdr_img(i,j,channel) = 0;
-            for k=1:img_size
-                %for l=1:channels
-                %    if(l==channel || channel==-1)
-                        pixel = imgs(k,i,j,channel);
-                        %
-                        hdr_img(i,j,channel) = hdr_img(i,j,channel) + weight(pixel+1)*(g(pixel+1)-log(Time(k)));
-                        weight_total(i,j) = weight_total(i,j) + weight(pixel+1);
-               %     else
-                %        pixel = imgs(k,i,j,l);%(k
-                        %new_pixel = hdr_img(i,j,l) + pixel/img_size;%weight(pixel+1);
-                 %       new_pixel = pixel/img_size;%weight(pixel+1);
-                 %       hdr_img(i,j,l) = new_pixel;
-                 %   end
-                %end
+        
+    
+    for k=1:img_size
+        %compute weights using weightfunc
+        img(:,:) = squeeze(imgs(k,:,:,channel));
+        parfor i=1:img_w
+            for j=1:img_h
+                weight(i,j) = weightfunc(img(i,j)+1);
             end
         end
+        if useRef > 0
+            %de-ghost by filtering weights based on correlation with reference image
+            weight(:,:) = DeGhostWeights(weight(:,:),Irr(:,:,k),IrrRef,100, ghostthresh);
+        end
+        
+        %add weighted value to totals for each pixel
+        parfor i=1:img_w
+            for j=1:img_h
+                hdr_img(i,j,channel) = hdr_img(i,j,channel) + weight(i,j)*(Irr(i,j,k));
+                weight_total(i,j) = weight_total(i,j) + weight(i,j);
+            end
+        end
+        
+        disp(['Deghosting complete for image ',num2str(k)]);
     end
     
     for i=1:img_w
         for j=1:img_h
-            %if(channel==-1)
-            %    for l=1:channels
-            %        hdr_img(i,j,l) = hdr_img(i,j,l)/weight_total(i,j);
-           %     end
-           % end
-           % if (channel>0)
            if(weight_total(i,j) ==0)
                hdr_img(i,j,channel) = 1;
            else
                 hdr_img(i,j,channel) = hdr_img(i,j,channel)/weight_total(i,j);
            end
-                %if(hdr_img(i,j,channel)<0)
-                %    disp(hdr_img(i,j,channel));
-                %end
-           % end
         end
     end
 end
